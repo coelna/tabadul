0e38c775e9f36afa825776153651ed05e4b79cb0
coelna <coelna@hotmail.de>
2016-05-19 21:49:49 +0200

    lect1 added lueckentext aufgabe

--- app/scripts/controllers/lect.js
+++ app/scripts/controllers/lect.js
@@ -1,4 +1,18 @@
 'use strict';
 
+
+/*angular.module('tabadulApp', ['lvl.directives.dragdrop'])
+  .controller('lectCtrl', ['$scope', function($scope) {
+    $scope.dropped = function(dragEl, dropEl) {
+      // this is your application logic, do whatever makes sense
+      //var drag = angular.element(dragEl);
+      //var drop = angular.element(dropEl);
+
+      //console.log('The element ' + drag.attr('id') + ' has been dropped on ' + drop.attr('id') + '!');
+    };
+  }]);
+*/
+'use strict';
+
 angular.module('tabadulApp')
   .controller('lectCtrl', function () {});
