3f7514a18f010ab3ed5b48ff8b2df95a554e1dd7
coelna <coelna@hotmail.de>
2016-05-21 01:26:17 +0200

    refactoring

--- app/scripts/controllers/lect.js
+++ app/scripts/controllers/lect.js
@@ -1,18 +1,41 @@
 'use strict';
 
-
-/*angular.module('tabadulApp', ['lvl.directives.dragdrop'])
-  .controller('lectCtrl', ['$scope', function($scope) {
-    $scope.dropped = function(dragEl, dropEl) {
-      // this is your application logic, do whatever makes sense
-      //var drag = angular.element(dragEl);
-      //var drop = angular.element(dropEl);
-
-      //console.log('The element ' + drag.attr('id') + ' has been dropped on ' + drop.attr('id') + '!');
-    };
-  }]);
-*/
-'use strict';
-
 angular.module('tabadulApp')
   .controller('lectCtrl', function () {});
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+
+  if(ev.target.childNodes.length > 0) {
+    return;
+  }
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+
+  if(document.getElementById("divtarget1").hasChildNodes() && document.getElementById("divtarget2").hasChildNodes())
+  {
+    document.getElementById("ueberpruefen").removeAttribute("disabled");
+  }
+}
+
+function ueberpruefeTexte () {
+  var target1 = document.getElementById("divtarget1").firstElementChild.innerHTML;
+  var target2 = document.getElementById("divtarget2").firstElementChild.innerHTML;
+  var richtig1 = "worden ist";
+  var richtig2 = "dass";
+  if(target1 == richtig1 && target2 == richtig2){
+    document.getElementById("result").innerHTML = "Alles richtig!";
+  }
+  else {
+    document.getElementById("result").innerHTML = "Leider falsch.";
+  }
+}
+document.getElementById("ueberpruefen").onclick = ueberpruefeTexte;
